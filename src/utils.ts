b1ee816b49ad1527472978b4872b7508a5c38211
Florent Le Gall <florent.legall@gmail.com>
2019-07-25 21:29:47 +0200

    rewrite ioTypeCheck()

--- src/utils.ts
+++ src/utils.ts
@@ -3,23 +3,21 @@ import { Type } from "io-ts";
 import { PathReporter } from "io-ts/lib/PathReporter";
 import { isLeft } from "fp-ts/lib/Either";
 
-export function ioTypeCheck<A, O, I>(
-  type: Type<A, O, I>,
-): (value: unknown) => A {
-  return (value: unknown) => {
-    const valueOrError = type.decode(value as any);
-    if (isLeft(valueOrError)) {
-      const error = new Error(
-        `ioTypeCheck error(s): ${PathReporter.report(valueOrError).join(
-          ",\n",
-        )} : value : "${JSON.stringify(value)}"`,
-      );
-      throw error;
-    } else {
-      return valueOrError.right;
-    }
-  };
-}
+export const ioTypeCheck = <A, O, I>(type: Type<A, O, I>) => (
+  value: unknown,
+): A => {
+  const valueOrError = type.decode(value as any);
+  if (isLeft(valueOrError)) {
+    const error = new Error(
+      `ioTypeCheck error(s): ${PathReporter.report(valueOrError).join(
+        ",\n",
+      )} : value : "${JSON.stringify(value)}"`,
+    );
+    throw error;
+  } else {
+    return valueOrError.right;
+  }
+};
 
 export const useTypeChecker = <A, O, I>(
   type: Type<A, O, I>,
