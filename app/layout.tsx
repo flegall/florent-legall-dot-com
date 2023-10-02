8f66c70f619ffb4928bf39a27d7a5e5b1a9ee5ee
Florent Le Gall <florent.legall@datadoghq.com>
2023-10-02 15:12:04 +0200

    Improve layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,26 @@
-import "./globals.css";
+import cx from "classnames";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
+
+import styles from "./layout.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const robotoTitle = Roboto({
+  weight: "700",
+  subsets: ["latin"],
+});
+
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+});
+
 export const metadata: Metadata = {
-  title: "Florent Le Gall's blog",
-  description: "My blog !",
+  title: "Software's kitchen",
+  description:
+    "Notes on software development in english and sometimes in french.",
 };
 
 export default function RootLayout({
@@ -16,7 +30,39 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cx(inter.className, styles.body)}>
+        <header className={cx(robotoTitle.className, styles.header)}>
+          Software kitchen
+        </header>
+        <main className={cx(roboto.className, styles.main)}>
+          <div className={styles.mainContent}>{children}</div>
+          <div className={styles.signatureRow}>
+            {/* eslint-disable-next-line @next/next/no-img-element*/}
+            <img
+              alt="Florent Le Gall"
+              src="//www.gravatar.com/avatar/fd5e5220fa103cc75e2f4f9fbe16ebb7?d=retro&amp;r=g&amp;s=100"
+              srcSet="//www.gravatar.com/avatar/fd5e5220fa103cc75e2f4f9fbe16ebb7?d=retro&amp;r=g&amp;s=200 2x"
+              height="100"
+              width="100"
+              className={styles.avatar}
+            />
+            <p className={styles.signatureText}>
+              <b className={robotoTitle.className}>
+                Florent Le Gall&apos;s personal blog
+              </b>
+              <br />
+              Notes on software development in english and sometimes in french.
+            </p>
+          </div>
+          <div className={styles.links}>
+            <span>
+              <a href="https://twitter.com/flornt">twitter</a> •{" "}
+              <a href="https://github.com/flegall">github</a> •{" "}
+              <a href="https://www.linkedin.com/in/flegall/">linkedin</a>
+            </span>
+          </div>
+        </main>
+      </body>
     </html>
   );
 }
